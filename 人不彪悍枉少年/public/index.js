b2b82dd41132e260b5d2227504a7df46f8783114
Zheng-Luxi <2092028826@qq.com>
2020-07-21 15:34:20 +0800

    人不彪悍枉少年

--- 人不彪悍枉少年/public/index.js
+++ 人不彪悍枉少年/public/index.js
@@ -12,5 +12,9 @@ eval(atob("dmlkZW9zID0gdmlkZW9zLm1hcCggaXRlbSA9PiBgaHR0cHM6Ly9va2p4LmNjP3VybD0ke
 		iframe.src = videos[[...document.querySelectorAll("li")].findIndex(cur=>cur==item)];
 		iframe.width = "100%";
 		iframe.height = "100%";
+		[...document.querySelectorAll("li")].forEach(item=>{
+			item.className = "";
+		});
+		e.target.className = "select";
 	});
 });
